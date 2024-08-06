adbd946244c3c2318faf8b632e1772b2086c29a3
kedong <kedong@yunlizhi.com>
2024-08-06 17:57:30 +0800

    增加命令行参数

--- uploadFileToMinio/main.go
+++ uploadFileToMinio/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"minioUploadFile/upload"
 	"os"
 )
 
 func main() {
 	if len(os.Args) == 1 {
-		//fmt.Println("缺少参数")
-		// return
+		fmt.Println("缺少参数")
+		return
 	}
-	// localFile := os.Args[1]
-	// minioPath := os.Args[2]
-	localFile := "./upload/upload.go"
-	minioPath := "/abc"
+	localFile := os.Args[1]
+	minioPath := os.Args[2]
+	// localFile := "./upload/upload.go"
+	// minioPath := "/abc"
 
 	// 上传错误信息
 	// upload.UploadError(localFile, minioPath)
