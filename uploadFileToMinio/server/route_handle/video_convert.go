9e635a16d544c0265b4fee03d21eee1a64580cf8
kedong <kedong@yunlizhi.com>
2024-08-08 13:53:28 +0800

    视频转码后删除临时文件

--- uploadFileToMinio/server/route_handle/video_convert.go
+++ uploadFileToMinio/server/route_handle/video_convert.go
@@ -25,19 +25,29 @@ func Mp42m3u8(w http.ResponseWriter, r *http.Request) {
 		common.ERROR_DATA(w, "保存文件失败", err)
 		return
 	}
+	// 上传完后删除临时文件
+	defer func() {
+		log.Println("删除视频临时文件", filePath)
+		os.Remove(filePath)
+	}()
 
 	hlsDir := os.Getenv("HOME") + "/tmp/hls"
 
 	// 视频转码
-	m3u8File, dir, err := video.Convert(filePath, hlsDir)
+	m3u8File, m3u8Dir, err := video.Convert(filePath, hlsDir)
 	if err != nil {
 		log.Fatal("视频转码失败", err)
 		common.ERROR_DATA(w, "视频转码失败", err)
 		return
 	}
+	// 上传完后删除临时目录
+	defer func() {
+		log.Println("删除 m3u8 临时文件夹", m3u8Dir)
+		os.RemoveAll(m3u8Dir)
+	}()
 
 	// 将m3u8 文件上传至minio
-	upload.MultipartUpload(dir, dir)
+	upload.MultipartUpload(m3u8Dir, m3u8Dir)
 
 	// minioPath
 	url := "/" + upload.BucketName + "/" + m3u8File
